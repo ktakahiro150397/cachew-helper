1144b785f581c9ff5d64bfd85bb6e31e5cf01a1b
Logic-Koike <koike@logic-biz.co.jp>
2025-06-25 13:21:22 +0900

    バックアップ重複を除外するよう修正

--- src/handler/export-backup.ts
+++ src/handler/export-backup.ts
@@ -19,11 +19,11 @@ export function exportBackup(): void {
   }
 
   // CashewImportシートのデータを取得
-  const dataRange = cashewImportSheet.getRange("A:G");
+  const dataRange = cashewImportSheet.getRange("A:H");
   const dataValues = dataRange.getValues();
 
   // バックアップシートのA:Hのうちどれかにデータが入っている最終行を取得
-  const backupValues = backupSheet.getRange("A:G").getValues();
+  const backupValues = backupSheet.getRange("A:H").getValues();
   let lastRow = 0;
   for (let i = 0; i < backupValues.length; i++) {
     if (backupValues[i].some((cell) => cell !== "" && cell !== null)) {
@@ -36,11 +36,26 @@ export function exportBackup(): void {
   // バックアップシートにデータを追記
   if (dataValues.length > 1) {
     // ヘッダー行を除くため、1行以上のデータがある場合のみ追記
+
+    // H列のバックアップキーが存在する場合は対象外とする
+    const backupKeys = backupValues.map((row) => row[7]); // H列の値を取得
+
+    // データ書き込みはA:G列のみ
+    const newData = dataValues
+        .slice(1)
+        .filter((row) => !backupKeys.includes(row[7]))
+        .map((row) => row.slice(0, 7)); // H列を除外
+
+    if (newData.length === 0) {
+      Logger.log("新しいデータはありません。バックアップは行われませんでした。");
+      return;
+    }
+
     backupSheet
-      .getRange(lastRow + 1, 1, dataValues.length - 1, dataValues[0].length)
-      .setValues(dataValues.slice(1));
+      .getRange(lastRow + 1, 1, newData.length, newData[0].length)
+      .setValues(newData);
     Logger.log(
-      `バックアップシートに ${dataValues.length - 1} 行のデータを追記しました。`
+      `バックアップシートに ${newData.length} 行のデータを追記しました。`
     );
   } else {
     Logger.log(
